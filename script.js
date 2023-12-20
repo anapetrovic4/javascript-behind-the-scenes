ecdb689946dad881b7820a71ab10706bc99c183f
Ana Petrovic <petrovic.bs.ana@gmail.com>
2023-12-20 15:00:35 +0100

    hoisting and TDZ

--- script.js
+++ script.js
@@ -15,16 +15,13 @@ function calcAge(birthYear) {
 
             //redefining output variable
             const output = 'NEW OUTPUT!';
-            
+
             const str = `Oh, and you're a millenial, ${firstName}`;
             console.log(str);
 
             function add(a, b){
                 return a + b;
             }
-
-
-
         }
         console.log(millenial);
         //add(2, 3);
@@ -39,3 +36,44 @@ function calcAge(birthYear) {
 //firstName je globalna varijabla, dostupna svugde u kodu
 const firstName = 'Jonas';
 calcAge(1991);
+
+//Hoisting with variables
+console.log(me);
+//console.log(job);
+//console.log(year);
+
+var me = 'Ana';
+let job = 'student';
+const year = 2000;
+
+//Hoisting with functions
+console.log(addDecl(2, 3));
+//console.log(addExpr(2, 3));
+//console.log(addArrow(2, 3));
+
+function addDecl(a, b){
+    return a + b;
+}
+
+const addExpr = function (a, b){
+    return a + b;
+}
+
+var addArrow = (a, b) => a + b;
+
+//Example
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart(items){
+    console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
